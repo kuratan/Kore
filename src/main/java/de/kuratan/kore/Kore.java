e24f6e6c1a6b30aaf82495bc1dabd9f2147df181
Kuraron <kuraron.kuratan@gmail.com>
2015-07-25 19:45:59 +0200

    .

--- src/main/java/de/kuratan/kore/Kore.java
+++ src/main/java/de/kuratan/kore/Kore.java
@@ -4,10 +4,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.network.NetworkRegistry;
 import de.kuratan.kore.lib.Reference;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
 
 
 @Mod(modid = Reference.MOD_ID, version = Reference.MOD_VERSION, name = Reference.MOD_NAME)
