438adb270fcde0536775b70632180e69cfa4c6c6
Kuraron <kuraron.kuratan@gmail.com>
2015-07-25 19:37:21 +0200

    Initial commit

---
+++ src/main/java/de/kuratan/kore/Kore.java
@@ -0,0 +1,30 @@
+package de.kuratan.kore;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import de.kuratan.kore.lib.Reference;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+
+@Mod(modid = Reference.MOD_ID, version = Reference.MOD_VERSION, name = Reference.MOD_NAME)
+public class Kore {
+
+    @Mod.Instance(Reference.MOD_ID)
+    public static Kore instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+}
